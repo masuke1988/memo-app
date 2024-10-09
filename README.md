335ad8dfc11de4d5883c50a5835e3c1f36e117f6
msdkisk <masukemassun@gmail.com>
2024-10-09 14:13:04 +0900

    README.md更新、アプリ作成

--- README.md
+++ README.md
@@ -1,36 +1,6 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+## Next.js　練習用アプリ メモ帳
 
-## Getting Started
-
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+- 入力欄に文章を入力→メモを追加で入力欄下に、入力した内容が表示される
+- データはlocalstorageに保存される
+- 追加したメモの編集も可能
+- 装飾はほぼ行っていない。tailwindを使用し、中心あたりに表示されるように調整している
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,93 @@
-import Image from "next/image";
+'use client'; //クライアントコンポーネントの指定が必要
+
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  // メモ用のステート
+  const [notes, setNotes] = useState([])
+
+  // 新しいメモを入力するためのステート
+  const [newNote, setNewNote] = useState('');
+
+  // 編集中のメモのインデックスを保持するステート
+  const [editIndex, setEditIndex] = useState(null);
+
+  useEffect(() => {
+    const storedNotes = JSON.parse(localStorage.getItem('notes')) || [];
+    if (storedNotes) {
+      setNotes(storedNotes); // ローカルストレージからメモを取得
+    }
+  }, []);
+
+  // notes配列が更新されるたびにローカルストレージに保存する
+  useEffect(() => { 
+    if (notes.length > 0) {
+      localStorage.setItem('notes', JSON.stringify(notes));
+    }
+  }, [notes])
+
+
+  const handleAddNote = () => {
+    if (newNote.trim() === '') return //空のメモを追加しないようにする
+
+    // 編集中のメモがある場合。
+    if (editIndex !== null) {
+      const updatedNotes = [...notes]
+      console.log(updatedNotes)
+      updatedNotes[editIndex] = { content: newNote }
+      setNotes(updatedNotes)
+      console.log("リセット前" + editIndex)
+      setEditIndex(null)
+      
+    } else {
+      setNotes([...notes, { content: newNote }]) //新しいメモを配列に追加
+    }
+    setNewNote('') //新しいメモを追加したら、入力欄を空にする
+  }
+
+  // 
+  const handleEditNote = (index: number) => {
+    setNewNote(notes[index].content)
+    setEditIndex(index)
+  }
+
+  const handleDeleteNote = (index: number) => {
+    // index番目のメモを削除
+    // _: 現在処理している要素そのもの（使っていないので、アンダースコアで無視）。
+    // i: 現在処理している要素のインデックス
+    const updatedNotes = notes.filter((_, i) => i !== index)
+    setNotes(updatedNotes)
+  }
+
+  
+
+  return (
+    <div className='p-20 flex flex-col align-center gap-10'>
+      <h1 className="text-center">メモアプリ</h1>
+      <input
+        className='text-zinc-900'
+        type="text"
+        value={newNote}
+        onChange={(e) => {
+          setNewNote(e.target.value)
+        }}
+        placeholder="メモを入力"
+      />
+      <button onClick={handleAddNote}>
+        {editIndex !== null ? 'メモを更新' : 'メモを追加'}
+      </button>
+      <ul>
+        {notes.map((note, index) => (
+          <li key={index} className="flex items-center gap-5">
+            <span>{note.content}</span>
+            <div className="min-w-max">
+              <button className="ml-auto mr-3" onClick={() => handleEditNote(index)}>編集</button>
+              <button onClick={() => handleDeleteNote(index)}>削除</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
